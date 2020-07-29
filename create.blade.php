c653a4260c9901144cb2a4a15b8f5a00cccc25c5
海野裕 <u.hiroshi19920103@gmail.com>
2020-07-29 13:46:54 +0900

    first commit

---
+++ create.blade.php
@@ -0,0 +1,17 @@
+
+@extends('layouts.profile')
+
+
+
+@section('title', '課題の作成')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>課題の作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
